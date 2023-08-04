6d309df7ffcc8c7e925eb4cbd8b0139eee260eb7
GolvenLD <47190446+GolvenLD@users.noreply.github.com>
2023-08-03 23:50:57 -0400

    Update cypher_parse.py to adapt to training dataset
    This version uses the cypher_example.csv file to correct the cypher and check it against the 'Correct Query'. With this test file, everything works as expected!

--- cypher_parse.py
+++ cypher_parse.py
@@ -1,83 +1,66 @@
 import re
 import pandas as pd
 
-cypher_path = "cypher.cyp"
-output_path = "corrected_cypher.cyp"
 
-try:
-    with open(cypher_path, "r") as file:
-        cypher_statement = file.read()
-except FileNotFoundError:
-    print("File not found. Please check the file path.")
-
-
-def correct_cypher(schema, cypher_statement):
+def correct_cypher(schema, cypher_statement, correct_statement):
     new_cypher = cypher_statement
-    rel_creation_attempt_pattern = r'((CREATE|MERGE) \(([\w]+)?:?[\w$]+\)\s*<?-\[([\w]+)?:?([\w$]+)?\]->?\s*\(([\w]+)?:?[\w$]+\))'
-    matches = re.finditer(rel_creation_attempt_pattern, cypher_statement)
+    node_pattern = r'\(([\w]+)?:?[\w$]+(\s{.*})?\)'
+    arrow_pattern = r'<?-\[([\w]+)?:?([\w$]+)?(\s{.*})?\]->?'
+    rel_pattern = node_pattern+r'\s*'+arrow_pattern+r'\s*'+node_pattern+r'(\s*'+arrow_pattern+r'\s*'+node_pattern+r')?'
+    matches = list(re.finditer(rel_pattern, cypher_statement))
     if matches:
         for match in matches:
-            match_group = match.group()
-            match_span = list(match.span())
+            relationship = match if isinstance(match, str) else match.group()
 
-            relationship = match_group.split(' ')[1]
-            print("Relationship: "+relationship)
+            if relationship.count('-') > 2:
+                relation_bits = relationship.split(')')
+                relationship1 = relation_bits[0] + ')' + relation_bits[1] + ')'
+                relationship2 = '(' + relation_bits[1].split('(')[1] + ')' + relation_bits[2] + ')'
+                relationship = relationship1
+                matches.append(relationship2)
 
-            arrow_pattern = r'(<?-\[([\w]+)?:?([\w$]+)?\]->?)'
-            arrow = next(re.finditer(arrow_pattern, relationship)).group()
+            print(relationship)
+
+            try:
+                arrow = next(re.finditer(arrow_pattern, relationship)).group()
+            except:
+                print('No arrows found - check cypher.')
+
             arrow_dir = detect_arrow_direction(arrow, cypher_statement)
             print('Arrow: '+arrow+', Direction: '+str(arrow_dir))
 
             rel_type = extract_rel_type(arrow, cypher_statement)
-            print('Relationship type: '+rel_type)
 
-            node_pattern = r'(\(([\w]+)?:?[\w$]+\))'
             nodes = list(re.finditer(node_pattern, relationship))
-
-            (l_node, r_node) = (nodes[0].group(), nodes[1].group())
-            print("Left node: "+l_node)
-            print("Right node: "+r_node)
+            l_node = nodes[0].group()
+            r_node = nodes[1].group()
             l_node_type = extract_node_type(l_node, cypher_statement)
             r_node_type = extract_node_type(r_node, cypher_statement)
-            print("Left node type: "+l_node_type)
-            print("Right node type: "+r_node_type)
 
-            try:
-                if (arrow_dir == 1 and rel_type in schema[l_node_type][r_node_type]) or (arrow_dir == -1 and rel_type in schema[r_node_type][l_node_type]):
-                    print("Relationship direction fits schema!")
-                elif rel_type in schema[l_node_type][r_node_type] or rel_type in schema[r_node_type][l_node_type]:
-                    print("Relationship direction does not fit schema but could exist, needs to be corrected.")
+            schemas = schema.split('), ')
+            for index, schema in enumerate(schemas):
+                if index != len(schemas)-1:
+                    schemas[index] = schema+')'
+            if (arrow_dir == 1 and '('+l_node_type+', '+rel_type+', '+r_node_type+')' in schemas) or (arrow_dir == -1 and '('+r_node_type+', '+rel_type+', '+l_node_type+')' in schemas):
+                print('Relationship fits schema.')
+            else:
+                if (arrow_dir == 1 and '('+r_node_type+', '+rel_type+', '+l_node_type+')' in schemas) or (arrow_dir == -1 and '('+l_node_type+', '+rel_type+', '+r_node_type+')' in schemas):
+                    print('Direction change needed.')
                     if arrow_dir == 1:
                         new_arrow = '<'+arrow[:-1]
-                    elif arrow_dir == -1:
-                        new_arrow = arrow[1:]+'>'
                     else:
-                        if arrow[0] != '-':
-                            arrow = arrow[1:-1]
+                        new_arrow = arrow[1:]+'>'
 
-                        if rel_type in schema[l_node_type][r_node_type]:
-                            new_arrow = arrow+'>'
-                        else:
-                            new_arrow = '<'+arrow
                     print('New arrow: '+new_arrow)
-
-                    command = 'CREATE' if match_group[:6] == 'CREATE' else 'MERGE'
-                    new_match = command + ' ' + l_node + new_arrow + r_node
-                    new_cypher = new_cypher.replace(match_group, new_match)
+                    new_rel = l_node + new_arrow + r_node
+                    new_cypher = new_cypher.replace(relationship, new_rel)
+                elif arrow_dir == 0:
+                    print('No direction given in query; no changes are made.')
                 else:
-                    print("Error: One of the node or relationship types is not valid/in the schema. Review Cypher.")
-                    print("Line will be commented out.")
-                    new_match = '// ' + match_group
-                    new_cypher = new_cypher.replace(match_group, new_match)
+                    print('Relationship does not fit schema - watch out. No modifications made.')
 
-            except Exception as e:
-                print('Error: One of the nodes or relationships types is not valid/in the schema. Review Cypher.')
-                print("Line will be commented out.")
-                print(f'Error code: {e}')
-                new_match = '// ' + match_group
-                new_cypher = new_cypher.replace(match_group, new_match)
-
-            print()
+    print(new_cypher == correct_statement)
+    print()
     return new_cypher
 
 
@@ -92,51 +75,41 @@ def detect_arrow_direction(arrow, cypher_statement):
 
 
 def extract_rel_type(arrow, cypher_statement):
-    type_pattern = r'(:[\w$]+\])'
+    type_pattern = r'(:[\w$]+(\s{.*})?\])'
     if ':' in arrow:
-        type = next(re.finditer(type_pattern, arrow)).group()[1:-1]
-        print(type)
+        type = next(re.finditer(type_pattern, arrow)).group()[1:-1].split(' ')[0]
         return type
     else:
-        rel_name_pattern = r'(\[[\w$]\])'
+        rel_name_pattern = r'(\[[\w$](\s{.*})?\])'
         rel_name = next(re.finditer(rel_name_pattern, arrow)).group()[1:-1]
-        print('rel_name '+rel_name)
-        rel_pattern = r'(\['+rel_name+':[\w$]+\])'
+        rel_pattern = r'(\['+rel_name+':[\w$]+(\s{.*})?\])'
         matches = list(re.finditer(rel_pattern, cypher_statement))
         if matches:
             match = matches[0].group()
             if ':' in match:
-                return next(re.finditer(type_pattern, match)).group()[1:-1]
+                return next(re.finditer(type_pattern, match)).group()[1:-1].split(' ')[0]
         else:
             return 'No type'
 
 
 # TODO: clean up the patterns
 def extract_node_type(node, cypher_statement):
-    type_pattern = r'(:[\w$]+\))'
+    type_pattern = r'(:[\w$]+(\s{.*})?\))'
     if ':' in node:
-        return re.findall(type_pattern, node)[0][1:-1]
+        return re.findall(type_pattern, node)[0][0][1:-1].split(' ')[0]
     else:
         node_name = node[1:-1]
-        node_pattern = '(\('+node_name+':[\w$]+\))'
-        matches = re.findall(node_pattern, cypher_statement)
+        node_pattern = '(\('+node_name+':[\w$]+(\s{.*})?\))'
+        matches = list(re.findall(node_pattern, cypher_statement))
         if len(matches) != 0:
-            return re.findall(type_pattern, matches[0])[0][1:-1]
+            print(matches[0][0])
+            node_type = re.findall(type_pattern, matches[0][0])[0][0][1:-1].split(' ')[0]
+            return node_type
         else:
             return 'No type'
 
 
-schema_data = {'index': ['Person', 'Movie', 'Equipment'],
-               'Person': [[],['ACTED_IN','PRODUCED'],['USED']],
-               'Movie': [[],[],['REQUIRED']],
-               'Equipment':[[],[],[]]}
-
-schema_df = pd.DataFrame.from_dict(schema_data).set_index('index')
-
-corrected_cypher_statement = correct_cypher(schema_df, cypher_statement)
-try:
-    with open(output_path, "w") as output_file:
-        output_file.write(corrected_cypher_statement)
-    print("Corrected cypher written to new_cypher.cyp file.")
-except Exception as e:
-    print(f"Error writing to the file: {e}")
+test_dataset = pd.read_csv('cypher_examples.csv')
+for row in test_dataset.iterrows():
+    print(row[1][0])
+    corrected_row = correct_cypher(row[1][1], row[1][0], row[1][2])
